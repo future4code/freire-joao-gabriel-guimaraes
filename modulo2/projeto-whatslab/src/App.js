b0e4001291aa92356df8aa0380cef5d739b86c93
João Gabriel Guimarães <joaoguimaraes14@gmail.com>
2022-06-12 14:19:22 -0300

    fazendo algumas modificações para ficar mais bonito visualmente

--- modulo2/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-import Mensagem from './components/Mensagem/Mensagem'
-import { createGlobalStyle } from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import Mensagem from "./components/Mensagem/Mensagem";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -10,91 +10,117 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
-`
+`;
 
-const Container = styled.div `
-    display: flex;
-    margin: auto;
-    flex-direction: column;
-     justify-content: end;
-    flex: 1 1 0%;
-    max-width: 600px;
-    height: 100vh;
-    border: 1px solid black; 
-    
-`
+const Container = styled.div`
+  display: flex;
+  margin: auto;
+  flex-direction: column;
+  justify-content: end;
+  flex: 1 1 0%;
+  max-width: 600px;
+  height: 100vh;
+  border: 1px solid black;
+  background-color: #e5ddd5;
+`;
 
+const Input = styled.input`
+  width: 25%;
+  height: 5vh;
+  margin-right: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: white;
+  border: 1px solid white;
+  &:focus {
+    outline: none;
+  }
+`;
+const Input1 = styled.input`
+  width: 65%;
+  margin-right: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: white;
+  border: 1px solid white;
+  height: 5vh;
+  &:focus {
+    outline: none;
+  }
+`;
+const Botao = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 5px;
+  align-items: center;
+  width: 10%;
+  background-color: whitesmoke;
+  border: 1px solid whitesmoke;
+  &:hover {
+    background-color: darkgray;
+    border-color: darkgray;
+  }
+  &:active {
+    background-color: darkgray;
+    border-color: darkgray;
+  }
+`;
 
-const Input = styled.input `
-    width: 25%;
-    height: 5vh;
-`
-const Input1 = styled.input `
+const Container1 = styled.div`
+  display: flex;
+  padding: 12px;
+`;
 
-    width: 70%;
-`
-const Botao = styled.button `
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    
-    width: 7,5%;
-`
-
-const Container1 = styled.div `
-display: flex;
-
-
-`
-
-class App extends React.Component  {
-
+class App extends React.Component {
   state = {
     texto: [],
     valorInputNome: "",
     valorInputMensagem: "",
-  }
+  };
 
   enviarMensagem = () => {
     const novaMensagem = {
       nome: this.state.valorInputNome,
       mensagem: this.state.valorInputMensagem,
-    }
-    const novoMensagem = [...this.state.texto, novaMensagem]
-    this.setState({texto: novoMensagem})
-    this.setState({valorInputNome: ""})
-    this.setState({valorInputMensagem: ""})
-  }
+    };
+    const novoMensagem = [...this.state.texto, novaMensagem];
+    this.setState({ texto: novoMensagem });
+    this.setState({ valorInputMensagem: "" });
+  };
   onChangeNome = (event) => {
-    this.setState({valorInputNome: event.target.value})
-  }
+    this.setState({ valorInputNome: event.target.value });
+  };
   onChangeMensagem = (event) => {
-    this.setState({valorInputMensagem: event.target.value})
-  }
-  render(){
+    this.setState({ valorInputMensagem: event.target.value });
+  };
+  render() {
     const mensagens = this.state.texto.map((conversa) => {
-      return (
-        
-        <Mensagem
-        nome = {conversa.nome} mensagem = {conversa.mensagem}
-         />
-      )
-    })
-  return (
-    
-    <Container>
-      <GlobalStyle  />
+      return <Mensagem nome={conversa.nome} mensagem={conversa.mensagem} />;
+    });
+    return (
+      <Container>
+        <GlobalStyle />
         {mensagens}
         <Container1>
-        <Input placeholder={'nome'} onChange={this.onChangeNome} value = {this.state.valorInputNome} />
-        <Input1 placeholder={'mensagem'} onChange={this.onChangeMensagem} value = {this.state.valorInputMensagem} />
-        <Botao onClick={this.enviarMensagem}>Enviar</Botao>
+          <Input
+            placeholder={"Usuário"}
+            onChange={this.onChangeNome}
+            value={this.state.valorInputNome}
+            type="text"
+          />
+          <Input1
+            placeholder={"Mensagem"}
+            onChange={this.onChangeMensagem}
+            value={this.state.valorInputMensagem}
+            type="text"
+          />
+          <Botao onClick={this.enviarMensagem}>
+            <strong>Enviar</strong>
+          </Botao>
         </Container1>
-    </Container>
-    
-    
-     
-  );
+      </Container>
+    );
   }
 }
 
--- modulo2/projeto-whatslab/src/components/Mensagem/Mensagem.js
+++ modulo2/projeto-whatslab/src/components/Mensagem/Mensagem.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Container1 = styled.div `
-    display: flex;
-    max-width: 600px;
-    padding: 20px;
-   
-`
+const Container1 = styled.div`
+  display: flex;
+  max-width: 40%;
+  padding: 10px 15px;
+  background-color: ${(props) => {
+    if (props.local === "right") {
+      return "#dcf8c6";
+    } else {
+      return "whitesmoke";
+    }
+  }};
+  text-align: ${(props) => props.local};
+  align-self: ${(props) => {
+    if (props.local === "right") {
+      return "flex-end";
+    } else {
+      return "flex-start";
+    }
+  }};
 
+  flex-direction: column;
+  margin: 0 12px 5px 12px;
+  border: 1px solid whitesmoke;
+  border-radius: 7px;
+  box-shadow: 2px 2px darkgray;
+`;
 
-class Mensagem extends React.Component  {
+const Container2 = styled.div`
+  margin-bottom: 10px;
+`;
 
-  
-  render(){
-    
-  return (
-    <Container1>
-        
-      <p><strong>{this.props.nome}: </strong> {this.props.mensagem}</p>
-        
-    </Container1>
-  );
+class Mensagem extends React.Component {
+  render() {
+    let local;
+
+    if (this.props.nome === "eu") {
+      local = "right";
+    } else {
+      local = "left";
+    }
+
+    return (
+      <Container1 local={local}>
+        <Container2>
+          <strong> {this.props.nome} </strong>{" "}
+        </Container2>
+        {this.props.mensagem}
+      </Container1>
+    );
   }
 }
 
