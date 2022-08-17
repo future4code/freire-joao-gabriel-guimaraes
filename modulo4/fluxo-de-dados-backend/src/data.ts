a57f3b3e7ca3053e3e6a554909ff1a7333ea69a8
João Gabriel Guimarães <joaoguimaraes14@gmail.com>
2022-08-17 18:51:47 -0300

    fluxo de dados no backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,7 @@
+type product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const products: product[] = [{id: '1', name: 'garrafinha', price: 50}, {id: '2', name: 'vasilha', price: 25}, {id: '3', name: 'pote', price: 15}] 
